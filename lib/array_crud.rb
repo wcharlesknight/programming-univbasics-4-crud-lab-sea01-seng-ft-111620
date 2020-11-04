1d330e8c9f21490757480b7d68f87d35ab80d9d0
Charlie Knight <williamchknight@gmail.com>
2020-11-04 22:24:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ['a','b','c','d']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << 'e'
 end
 
 def add_element_to_start_of_array(array, element)
